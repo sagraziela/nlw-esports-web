cd23ae402de018a3c9adc5856baedf52f7eceb76
Graziela Antunes <graziela.atns@gmail.com>
2022-09-17 17:16:13 -0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import './styles/main.css';
+import { useEffect, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog'
+import logoImg from './assets-style/logo-esports.svg';
+import { GameBanner } from './components/gameBanner';
+import { CreateAdBanner } from './components/createAdBanner';
+import { CreateAdModal } from './components/createAdModal';
+import axios from 'axios';
+
+interface Game {
+  id: string,
+  title: string,
+  bannerUrl: string,
+  _count: {
+    ads: number,
+  }
+}
+
+function App() {
+
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    axios('http://localhost:3333/games').then(response => {
+        setGames(response.data)
+    })
+  }, [])
+
+  return <div className="max-w-[1300px] mx-auto flex flex-col items-center my-20">
+    <img src={logoImg} alt="" />
+
+    <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui</h1>
+
+    <div className="grid grid-cols-6 gap-6 mt-16">
+
+      {games.map(game => {
+        return(
+          <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl} 
+            title={game.title} 
+            adsCount={game._count.ads}/>
+        )
+      })}
+
+    </div>
+
+    <Dialog.Root>
+      <CreateAdBanner />
+
+      <CreateAdModal />
+    </Dialog.Root>
+  </div>
+}
+
+export default App
